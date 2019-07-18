3aeb372da658bfd232373c354ecbb3f4123f962e
Andrew <andrewpeliza@hotmail.com>
2019-07-18 20:58:22 +0100

    More stepup files, database model, routes/config file which is hidden

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(express.json({ extended: false }));
 
