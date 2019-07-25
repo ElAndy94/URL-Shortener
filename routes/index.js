efd4344a39b4e57c142653e665f7ee7848d7191c
Andrew <andrewpeliza@hotmail.com>
2019-07-25 16:42:47 +0100

    Check if url code is stored, if so redirect.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const Url = require('../models/Url');
+
+// @roue    GET /:code
+// @desc    Redirect to long/original Url
+router.get('/:code', async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlCode: req.params.code });
+
+    if (url) {
+      return res.redirect(url.longUrl);
+    } else {
+      return res.status(404).json('No url found');
+    }
+  } catch (err) {
+    console.error(err);
+    res, status(500).json('Server Error');
+  }
+});
+
 module.exports = router;
